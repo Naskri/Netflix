cba25d26ddee18973b8de8e096c87b57c72475ae
Naskri <naskri.kamil@wp.pl>
2023-11-01 15:51:08 +0100

    feat: remove search
    Remove search UI and functionality.
    Refix a styles.

--- src/features/Layout/DashboardNavigation/DashboardNavigation.tsx
+++ src/features/Layout/DashboardNavigation/DashboardNavigation.tsx
@@ -1,23 +1,17 @@
 import { Logo } from '../../UI/Logo/Logo'
 import { Navigation } from '../Navigation/Navigation'
 import { NavigationActions } from '../Navigation/NavigationActions/NavigationActions'
-import { Search } from '../../UI/Search/Search'
 import { Avatar } from '../../UI/Avatar/Avatar'
 import { DashboardNavigationList } from './DashboardNavigationList/DashboardNavigationList'
 import styled from './DashboardNavigation.module.css'
-import { useMediaQuery } from '../../../hooks/useMediaQuery'
 
 export const DashboardNavigation = () => {
-  const matches = useMediaQuery('(max-width:36rem')
-
   return (
     <div className={styled['dashboard-navigation']}>
       <Navigation modifier="dashboard">
         <Logo />
-        {matches && <Search />}
         <DashboardNavigationList />
         <NavigationActions>
-          {!matches && <Search />}
           <Avatar />
         </NavigationActions>
       </Navigation>
